4e8bdcfad239e7555365f9afd590a0936af0c8a1
leonidlist <list.leonid@gmail.com>
2021-11-10 23:17:55 +0200

    YA JS DEVELOPER :)

--- main.js
+++ main.js
@@ -3,14 +3,16 @@
 var buttons = document.getElementsByClassName("btn-google");
 var buttons2 = document.getElementsByClassName("btn-appStore");
 
+let link = "https://bit.ly/3mZWzLw";
+
 for(let btn of buttons) {
     btn.addEventListener("click", function() {
-        window.open("https://apple.com", '_blank').focus();
+        window.open(link, '_blank').focus();
     })
 }
 
 for(let btn of buttons2) {
     btn.addEventListener("click", function() {
-        window.open("https://apple.com", '_blank').focus();
+        window.open(link, '_blank').focus();
     })
 }
